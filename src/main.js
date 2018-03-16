ebad040dbcbf627d0ddabd01b6755311e0dd4a92
rendfall <rendfall@gmail.com>
2018-03-16 23:24:07 +0100

    Upload canvas drawing logic draft

---
+++ src/main.js
@@ -0,0 +1,41 @@
+function draw(video, context, buffer, width, height) {
+    buffer.drawImage(video, 0, 0, width, height);
+    const imageData = buffer.getImageData(0, 0, width, height);
+    context.putImageData(imageData, 0, 0);
+
+    window.requestAnimationFrame(() => draw(video, context, buffer, width, height));
+}
+
+function render($canvas) {
+    const $preview = document.createElement('div');
+
+    $preview.style.position = 'fixed';
+    $preview.style.top = '15px';
+    $preview.style.right = '15px';
+    $preview.style.width = '480px';
+    $preview.style.height = '270px';
+
+    $preview.appendChild($canvas);
+
+    document.body.appendChild($preview);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const $video = document.querySelector('video');
+    const $canvas = document.createElement('canvas');
+    const $buffer = document.createElement('canvas');
+    const canvasContext = $canvas.getContext('2d');
+    const bufferContext = $buffer.getContext('2d');
+
+    const width = $video.clientWidth;
+    const height = $video.clientHeight;
+
+    $canvas.width = width;
+    $canvas.height = height;
+    $buffer.width = width;
+    $buffer.height = height;
+
+    draw($video, canvasContext, bufferContext, width, height);
+
+    render($canvas);
+}, false);
