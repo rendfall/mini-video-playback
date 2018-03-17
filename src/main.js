88e0a40a124ccfe11a3bab324a3ac220180cd578
rendfall <rendfall@gmail.com>
2018-03-17 01:27:11 +0100

    Extract loop logic + add hide feature

--- src/main.js
+++ src/main.js
@@ -2,15 +2,7 @@ const DEFAULT_CONFIG = {
     videoElement: null
 }
 
-function draw(video, context, buffer, width, height) {
-    buffer.drawImage(video, 0, 0, width, height);
-    const imageData = buffer.getImageData(0, 0, width, height);
-    context.putImageData(imageData, 0, 0);
-
-    window.requestAnimationFrame(() => draw(video, context, buffer, width, height));
-}
-
-function render($canvas) {
+function buildPreview() {
     const $preview = document.createElement('div');
 
     $preview.style.position = 'fixed';
@@ -20,9 +12,18 @@ function render($canvas) {
     $preview.style.height = '270px';
     $preview.style.zIndex = '2147483647';
 
-    $preview.appendChild($canvas);
+    return $preview;
+}
 
-    document.body.appendChild($preview);
+function draw(video, context, buffer, width, height) {
+    buffer.drawImage(video, 0, 0, width, height);
+    const imageData = buffer.getImageData(0, 0, width, height);
+    context.putImageData(imageData, 0, 0);
+}
+
+function render($target, $source) {
+    $target.appendChild($source);
+    document.body.appendChild($target);
 }
 
 function buildConfig(options) {
@@ -31,15 +32,36 @@ function buildConfig(options) {
     return new Map(entries);
 }
 
+class Loop {
+    constructor() {
+        this.id = null;
+    }
+
+    start(fn) {
+        this.step = () => {
+            fn.call(fn);
+            this.id = window.requestAnimationFrame(this.step);
+        }
+        this.step();
+    }
+
+    stop() {
+        window.cancelAnimationFrame(this.id);
+    }
+}
+
 // Expose API
 export class MiniVideoPlayback {
 
     constructor(options) {
+        this.$preview = buildPreview();
         this.config = buildConfig(options);
+        this.loop = new Loop();
     }
 
     show() {
         const $video = this.config.get('videoElement');
+        const $preview = this.$preview;
 
         if (!($video instanceof HTMLVideoElement)) {
             throw new Error(`The videoElement option must be HTMLVideoElement`);
@@ -58,8 +80,22 @@ export class MiniVideoPlayback {
         $canvas.width = width
         $canvas.height = height;
 
-        draw($video, canvasContext, bufferContext, width, height);
-        render($canvas); 
+        this.loop.start(() => {
+            draw($video, canvasContext, bufferContext, width, height);
+        });
+
+        render($preview, $canvas); 
     }
 
+    hide() {
+        this.$preview.style.display = 'none';
+        this.loop.stop();
+    }
+
+    destroy() {
+        if (this.$preview) {
+            this.preview.remove();
+            this.$preview = null;
+        }
+    }
 };
